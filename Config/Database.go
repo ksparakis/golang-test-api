b9b9c6d180be38891f20ef3e8ebda423a2b9c0d7
ksparakis <ksparakis@gmail.com>
2020-09-10 22:59:27 -0400

    fixed networking issue

--- Config/Database.go
+++ Config/Database.go
@@ -1,17 +1,24 @@
 package Config
 
 import (
+	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"os"
 )
 
 var DB *gorm.DB
 
 func BuildDB() (error){
-	//ip_address := os.Getenv("MYSQL_IP")
-	//dsn := fmt.Sprint("docker:docker@tcp(localhost:3306)/test_db?charset=utf8&parseTime=True&loc=Local", ip_address)
+	ip_address := os.Getenv("MYSQL_IP")
+	if (ip_address == ""){
+		ip_address = "localhost"
+	}
+	dsn := fmt.Sprintf("docker:docker@tcp(%s:3306)/test_db?charset=utf8&parseTime=True&loc=Local", ip_address)
+	fmt.Println(ip_address)
+	fmt.Printf(dsn)
 	initDb, err := gorm.Open(mysql.New(mysql.Config{
-		DSN: "docker:docker@tcp(localhost:3306)/test_db?charset=utf8&parseTime=True&loc=Local", // data source name
+		DSN: dsn,
 		DefaultStringSize: 256, // default size for string fields
 		DisableDatetimePrecision: true, // disable datetime precision, which not supported before MySQL 5.6
 		DontSupportRenameIndex: true, // drop & create when rename index, rename index not supported before MySQL 5.7, MariaDB
